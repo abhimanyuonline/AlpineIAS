7b77429b4b044fb1446ca6c4630c20c20d8f51b9
Abhimanyu <abhimanyu.nitdgp@gmail.com>
2025-12-14 15:32:08 +0530

    contact us feedback fix

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,7 +4,7 @@ let isInitialized = false;
 
 export function initEmailJS() {
   if (!isInitialized && typeof window !== 'undefined') {
-    emailjs.init('DEtpanQS847AcgU_4');
+    emailjs.init('eFqj0oAAYOabbFwSJ');
     isInitialized = true;
   }
 }
